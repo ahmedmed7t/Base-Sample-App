9e8150d7f5bacab55b876ce5760d61ce8e029b96
ahmedmed7at <medhatahmed19.am9@gmail.com>
2023-01-11 16:13:41 +0200

    add functions to generate random RRN

--- app/src/main/java/com/example/baseappsample/MainActivity.kt
+++ app/src/main/java/com/example/baseappsample/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.example.baseappsample
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val ex = (1..92).toList()
-        TestRandom().generateRandomNumber(1, 100, ex)
+        val ex = (0..98).toList()
+        val excludeArray = mutableListOf<Int>()
+
+        val end = 99
+        for (index in 1..100) {
+            val randomNumber = TestRandom().nextIntInRangeButExclude(0, end, excludeArray)
+        }
+
+        val x = TestRandom().numberPadding("111", 2, '0');
+
+        Log.v("Medhat  ", x)
     }
 }
--- app/src/main/java/com/example/baseappsample/TestRandom.java
+++ app/src/main/java/com/example/baseappsample/TestRandom.java
@@ -1,76 +1,93 @@
 package com.example.baseappsample;
 
-import android.util.Log;
-
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
 public class TestRandom {
-    Random randomGenerator = new Random();
-//    public int getRandomWithExclusion(int start, int end, List<Integer> exclude) {
-//        // very bad performance it loop sequentially
-//        Log.v("Medhat", "Start  "+System.currentTimeMillis());
-//        int random = start + new Random().nextInt(end - start + 1 - exclude.size());
-//        Log.v("Medhat", "random  "+random);
-//        for (int ex : exclude) {
-//            if (random < ex) {
-//                break;
-//            }
-//            random++;
-//            Log.v("Medhat", "random  "+random);
-//        }
-//        Log.v("Medhat", "return random  "+random);
-//        Log.v("Medhat", "End  "+System.currentTimeMillis());
-//        return random;
-//    }
-    /*
-      1. 48.498  -  48.501
-      2. 37.947  -  37.952
-    */
 
-    public Integer generateRandomNumber(int start, int end, List<Integer> excludeArray){
-        Log.v("Medhat", "Start  "+System.currentTimeMillis());
-
-        int rangeLength = end - start - excludeArray.size();
-        if(rangeLength <= 0){
-            excludeArray.clear();
-            int x =  randomGenerator.nextInt(end - start) + start;
-            Log.v("Medhat", "random  "+x);
-            return x;
+    /**
+     * this function will generate random number between two numbers
+     * the start and end and these two numbers are included in the range
+     * <p>
+     * it will throw exception if the start number is greater than the end since it will
+     * be a wrong range also end should be greater than 0
+     * <p>
+     * internally it will
+     * <p>
+     * - initiate exclude list if it's null
+     * <p>
+     * - clear exclude list if it's filled
+     * <p>
+     * - add generated numbers to the exclude list to avoid duplicate
+     * <p>
+     * - return random number
+     *
+     * @param start    - first integer can be generated randomly (the smallest number)
+     * @param end      - last integer can be generated randomly (the greatest number)
+     * @param excludes - previous generated numbers (validate that new number is not exist in that list)
+     *                 <p>
+     *                 ex:
+     *                 input(0, 99, [])
+     *                 output:
+     *                 - excludes = [5]
+     *                 - return = 5
+     */
+    public int nextIntInRangeButExclude(int start, int end, List<Integer> excludes) {
+        // method will generate random number including start and end
+        int size = end - start + 1; // this method works with size not end
+        if (start > end || end == 0)
+            throw new RuntimeException("Start should be less than end");
+        if (excludes == null) {
+            excludes = new ArrayList<>();
         }
-
-
-        int randomNumber;
-        do{
-            randomNumber = randomGenerator.nextInt(end - start) + start;
-            Log.v("Medhat", "random  "+randomNumber);
-        }while (excludeArray.contains(randomNumber));
-
-        excludeArray.add(randomNumber);
-        Log.v("Medhat", "End  "+System.currentTimeMillis());
-        return randomNumber;
-    }
-
-
-    public int nextIntInRangeButExclude(int start, int end, List<Integer> excludes){
-        Collections.sort(excludes);
-        int rangeLength = end - start - excludes.size();
-        if(rangeLength <= 0){
+        if (excludes.size() == size) {
+            // in case the exclude array filled
             excludes.clear();
-            rangeLength = end - start;
-            return new Random().nextInt(rangeLength) + start;
+            int randomNumber = new Random().nextInt(size - start) + start;
+            excludes.add(randomNumber);
+            return randomNumber;
         }
 
+        int rangeLength = size - excludes.size();
         int randomInt = new Random().nextInt(rangeLength) + start;
-        for(int i = 0; i < excludes.size(); i++) {
-            if(excludes.get(i) > randomInt) {
+        for (int i = 0; i < excludes.size(); i++) {
+            if (excludes.get(i) > randomInt) {
+                excludes.add(i, randomInt);
                 return randomInt;
             }
-
             randomInt++;
         }
+        excludes.add(randomInt);
         return randomInt;
+    }
+
+    /**
+     * This function will increase length of provided string number to
+     * the provided length that is paddingSize with a char provided that is
+     * paddingChar
+     *
+     * @param number      number that we need to add padding to it
+     * @param paddingSize size of number after padding
+     * @param paddingChar char that we need to add to the number as padding
+     *                    <p>
+     *                    Note: in case the number length is greater than paddingSize it will substring that
+     *                    number to th paddingSize
+     *                    <p>
+     *                    ex:
+     *                    input("123", 6, '0')  output: "123000"
+     *                    input("123456", 4, '0')  output: "1234"
+     */
+    public String numberPadding(String number, int paddingSize, char paddingChar) {
+        number = number.trim();
+        if (number.length() > paddingSize) {
+            return number.substring(0, paddingSize);
+        }
+        StringBuilder paddingNumber = new StringBuilder(number);
+        int missingChars = paddingSize - number.length();
+        for (int i = 0; i < missingChars; i++) {
+            paddingNumber.append(paddingChar);
+        }
+        return paddingNumber.toString();
     }
 }
